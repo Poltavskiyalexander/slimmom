06b74b5ddf7029c71046f8aa950bed180361f5e8
YaroslavKH1990 <yaroslav.kh.ua@gmail.com>
2021-06-16 00:25:31 +0300

    change small loader

---
+++ src/components/shared/SmallLoader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SmallLoader.scss';
+
+const SmallLoader = (props) => (
+	<div className="lds-ripple">
+		<div></div>
+		<div></div>
+	</div>
+);
+
+export default SmallLoader;
