208a3e2ec9f52b3fe5c61475d98abd3272242eeb
PoltavskiyAlexander <poltavskiyalexander@gmail.com>
2021-06-19 01:16:04 +0300

    add base

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,14 +11,30 @@ import {
  } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+import { loading, error } from '../redux/global/globalReducers';
+import authReducer from './auth/authReducer';
+import user from './user/userReducer';
+import alertReducer from './alert/alertReducer'
+
 const defaultMiddleware = getDefaultMiddleware({
   serializableCheck: {
     ignoredActions: [FLUSH, PAUSE, PERSIST, PURGE, REHYDRATE, REGISTER],
   },
 });
 
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['accessToken', 'refreshToken', 'sid']
+}
+
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    loading,
+    error,
+    user ,
+    alertReducer
   },
   middleware: [...defaultMiddleware, ],
 })
